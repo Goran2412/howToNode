ab70f37ec3a23225bf0dcac040453cf3673b7b83
Goran <gorandirkwestberg@gmail.com>
2023-08-03 14:01:52 -0300

    added maths file

--- main.js
+++ main.js
@@ -1,7 +1,16 @@
 // yeah this sucks
 // FUCK HELLO WORLD, EMBRACE SALUTATING
 
+const maths = require("./maths");
+
 const hi = 'hi ';
 
 const sayHello = (name) => console.log(hi + `${name}` + ". how you doin'?");
 sayHello("Goran");
+
+maths.sum(1, 2);
+maths.subtract(2, 1);
+maths.multiply(2, 2);
+maths.divide(4, 2);
+maths.divide(4, 0);
+maths.raise(2, 3);
