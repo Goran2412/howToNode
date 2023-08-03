e554de349c0e6ed256ca0f8f34412e1ff32ae3df
Goran <gorandirkwestberg@gmail.com>
2023-08-03 14:00:55 -0300

    idk what i am doing

--- main.js
+++ main.js
@@ -1,8 +1,6 @@
 // yeah this sucks
 // FUCK HELLO WORLD, EMBRACE SALUTATING
 
-const maths = require("./maths");
-
 const hi = 'hi ';
 
 const sayHello = (name) => console.log(hi + `${name}` + ". how you doin'?");
